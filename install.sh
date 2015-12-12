587fe6245cb7f2c3de8d353966b0a93029c5ef2a
Paul Dinh <paul.dinh@gmail.com>
2015-12-12 13:47:03 -0500

    Remove vim and konsole stuff

--- install.sh
+++ install.sh
@@ -5,22 +5,13 @@ read -p 'Continue (y/n)? ' -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	# remove previous settings
-	rm ~/.vimrc
-	rm -rf ~/.vim
 	mv ~/.gitignore_global ~/.gitignore_global.backup
 	mv ~/.config/terminator/config ~/.config/terminator/config.backup
 	mv ~/.bash_aliases ~/.bash_aliases.backup
-	mv ~/.kde/share/apps/konsole/DarkPastels.colorscheme ~/.kde/share/apps/konsole/DarkPastels.colorscheme.backup
-	mv ~/.kde/share/apps/konsole/Shell.profile ~/.kde/share/apps/konsole/Shell.profile.backup
 	mv ~/.tmux.conf ~/.tmux.conf.backup
 	mv ~/.tmux/dev ~/.tmux/dev.backup
 
 	echo ''
-	 create symlinks
-	ln -s ${PWD}/vim/vimrc ~/.vimrc;
-	echo 'Created symlink ~/.vimrc -> '${PWD}'/vim/vimrc'
-	ln -s ${PWD}/vim ~/.vim;
-	echo 'Created symlink ~/.vim -> '${PWD}'/vim/'
 	ln -s ${PWD}/.gitignore_global ~/.gitignore_global;
 	echo 'Created symlink ~/.gitignore_global -> '${PWD}'/.gitignore_global'
 	git config --global core.excludesfile ~/.gitignore_global
@@ -32,10 +23,6 @@ then
 	echo 'Created symlink ~/.bash_aliases -> '${PWD}'/.bash_aliases'
 	ln -s ${PWD}/.bash_profile ~/.bash_profile
 	echo 'Created symlink ~/.bash_profile -> '${PWD}'/.bash_profile'
-	ln -s ${PWD}/konsole/DarkPastels.colorscheme ~/.kde/share/apps/konsole/DarkPastels.colorscheme
-	echo 'Created symlink ~/.kde/share/apps/konsole/DarkPastels.colorscheme -> '${PWD}'/konsole/DarkPastels.colorscheme'
-	ln -s ${PWD}/konsole/Shell.profile ~/.kde/share/apps/konsole/Shell.profile
-	echo 'Created symlink ~/.kde/share/apps/konsole/Shell.profile -> '${PWD}'/konsole/Shell.profile'
 	ln -s ${PWD}/tmux/.tmux.conf ~/.tmux.conf
 	echo 'Created symlink ~/.tmux.conf -> '${PWD}'/tmux/.tmux.conf'
 	mkdir -p ~/.tmux
