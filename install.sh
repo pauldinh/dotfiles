666f31ff0a74702d1afe5706f788b5f43d67ddf2
Paul Dinh <paul.dinh@gmail.com>
2017-08-15 13:27:44 -0500

    Add termite config

--- install.sh
+++ install.sh
@@ -15,22 +15,33 @@ then
     echo ''
     ln -s ${PWD}/.gitignore_global ~/.gitignore_global;
     echo 'Created symlink ~/.gitignore_global -> '${PWD}'/.gitignore_global'
+
     git config --global core.excludesfile ~/.gitignore_global
     echo 'Set global git ignore to ~/.gitignore_global'
-    mkdir -p ~/.config/terminator
-    ln -s ${PWD}/terminator/config ~/.config/terminator/config
-    echo 'Created symlink ~/.config/terminator config -> '${PWD}'/terminator/config'
+
+    #mkdir -p ~/.config/terminator
+    #ln -s ${PWD}/terminator/config ~/.config/terminator/config
+    #echo 'Created symlink ~/.config/terminator config -> '${PWD}'/terminator/config'
+
     ln -s ${PWD}/.bash_aliases ~/.bash_aliases
     echo 'Created symlink ~/.bash_aliases -> '${PWD}'/.bash_aliases'
+
     ln -s ${PWD}/.bash_profile ~/.bash_profile
     echo 'Created symlink ~/.bash_profile -> '${PWD}'/.bash_profile'
+
     ln -s ${PWD}/tmux/.tmux.conf ~/.tmux.conf
     echo 'Created symlink ~/.tmux.conf -> '${PWD}'/tmux/.tmux.conf'
+
     mkdir -p ~/.tmux
     ln -s ${PWD}/tmux/dev ~/.tmux/dev
     echo 'Created symlink ~/tmux/dev -> '${PWD}'/tmux/dev'
+
     ln -s ${PWD}/vim/vimrc ~/.vimrc
     echo 'Created symlink ~/.vimrc -> '${PWD}'/vim/vimrc'
+
+    mkdir -p ~/.config/termite
+    ln -s ${PWD}/termite/config ~/.config/termite/config
+    echo 'Created symlink ~/.config/termite/config -> '${PWD}'/termite/config'
 
     # tmux
     mkdir -p ~/.tmux-layouts
