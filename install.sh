ba8b2b7012c332b529077cd5ba316ec5a8f64f67
Paul Dinh <paul.dinh@gmail.com>
2017-08-16 00:53:58 -0500

    More i3/polybar/compton changes

--- install.sh
+++ install.sh
@@ -12,39 +12,38 @@ then
     mv ~/.tmux/dev ~/.tmux/dev.backup
     mv ~/.vimrc ~/.vimrc.backup
 
-    echo ''
     ln -s ${PWD}/.gitignore_global ~/.gitignore_global;
-    echo 'Created symlink ~/.gitignore_global -> '${PWD}'/.gitignore_global'
 
     git config --global core.excludesfile ~/.gitignore_global
-    echo 'Set global git ignore to ~/.gitignore_global'
 
     #mkdir -p ~/.config/terminator
     #ln -s ${PWD}/terminator/config ~/.config/terminator/config
-    #echo 'Created symlink ~/.config/terminator config -> '${PWD}'/terminator/config'
 
     ln -s ${PWD}/.bash_aliases ~/.bash_aliases
-    echo 'Created symlink ~/.bash_aliases -> '${PWD}'/.bash_aliases'
 
     ln -s ${PWD}/.bash_profile ~/.bash_profile
-    echo 'Created symlink ~/.bash_profile -> '${PWD}'/.bash_profile'
 
     ln -s ${PWD}/tmux/.tmux.conf ~/.tmux.conf
-    echo 'Created symlink ~/.tmux.conf -> '${PWD}'/tmux/.tmux.conf'
 
     mkdir -p ~/.tmux
     ln -s ${PWD}/tmux/dev ~/.tmux/dev
-    echo 'Created symlink ~/tmux/dev -> '${PWD}'/tmux/dev'
 
     ln -s ${PWD}/vim/vimrc ~/.vimrc
-    echo 'Created symlink ~/.vimrc -> '${PWD}'/vim/vimrc'
+	mkdir -p ~/.vim/colors
+	ln -s ${PWD}/vim/space-vim-dark.vim ~/.vim/colors/space-vim-dark.vim
 
     mkdir -p ~/.config/termite
     ln -s ${PWD}/termite/config ~/.config/termite/config
-    echo 'Created symlink ~/.config/termite/config -> '${PWD}'/termite/config'
 
     ln -s ${PWD}/i3/i3status.conf ~/.i3status.conf
-    echo 'Created symlink ~/.i3status.conf -> '${PWD}'/i3/i3status.conf'
+
+    ln -s ${PWD}/i3/config ~/.config/i3/config
+
+    ln -s ${PWD}/compton/compton.conf ~/.config/compton.conf
+
+	mkdir -p ~/.config/polybar
+    ln -s ${PWD}/polybar/config ~/.config/polybar/config
+
 
     # tmux
     mkdir -p ~/.tmux-layouts
