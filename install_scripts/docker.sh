3b60c043182308fa0eb17d0bc901558a3a8f1b15
Paul Dinh <paul.dinh@gmail.com>
2020-01-09 16:30:13 -0600

    Add install_scripts/docker.sh

---
+++ install_scripts/docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# out with the old
+sudo apt remove docker docker-engine docker.io containerd runc
+
+sudo apt update
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# new shit
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
