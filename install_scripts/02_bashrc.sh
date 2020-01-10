47be9494f7f8cdc5f63344bbba1d155ff73935fd
Paul Dinh <paul.dinh@gmail.com>
2020-01-10 10:16:10 -0600

    Update .gitconfig stuff in 02_bashrc.sh

--- install_scripts/02_bashrc.sh
+++ install_scripts/02_bashrc.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "source ${HOME}/dotfiles/bash/.bash_profile" >> ${HOME}/.bashrc
+#echo "source ${HOME}/dotfiles/bash/.bash_profile" >> ${HOME}/.bashrc
 
-# git-aware-prompt
-mkdir -p ${HOME}/.bash && cd ${HOME}/.bash
-git clone https://github.com/pauldinh/git-aware-prompt.git
-echo "export GITAWAREPROMPT=~/.bash/git-aware-prompt" >> ${HOME}/.bashrc
-echo 'source "${GITAWAREPROMPT}/main.sh"' >> ${HOME}/.bashrc
+## git-aware-prompt
+#mkdir -p ${HOME}/.bash && cd ${HOME}/.bash
+#git clone https://github.com/pauldinh/git-aware-prompt.git
+#echo "export GITAWAREPROMPT=~/.bash/git-aware-prompt" >> ${HOME}/.bashrc
+#echo 'source "${GITAWAREPROMPT}/main.sh"' >> ${HOME}/.bashrc
 
-# diff-so-fancy (git)
-echo 'export PATH=${HOME}/dotfiles/diff-so-fancy:${PATH}' >> ${HOME}/.bashrc
+## diff-so-fancy (git)
+#echo 'export PATH=${HOME}/dotfiles/diff-so-fancy:${PATH}' >> ${HOME}/.bashrc
+
+# gitconfig
+ln -s ${HOME}/dotfiles/.gitconfig ${HOME}/.gitconfig
+ln -s ${HOME}/dotfiles/.gitignore_global ${HOME}/.gitignore_global
