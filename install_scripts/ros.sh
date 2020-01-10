e1ef38c2ccf202f4fd960b7f477e7d8c25a570d0
Paul Dinh <paul.dinh@gmail.com>
2020-01-10 15:49:58 -0600

    Set ROS_PYTHON_VERSION=2 in ros.sh

--- install_scripts/ros.sh
+++ install_scripts/ros.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-# sources and keys
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt update
+## sources and keys
+#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+#sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+#sudo apt update
 
-# ROS Melodic
-sudo apt install ros-melodic-desktop-full
+## ROS Melodic
+#sudo apt install ros-melodic-desktop-full
 
-# rosdep
-sudo rosdep init
-rosdep update
+## rosdep
+#sudo rosdep init
+#rosdep update
 
-# catkin tools and other shit
-sudo apt install \
-    python-rosinstall \
-    python-rosinstall-generator \
-    python-wstool \
-    build-essential \
-    python-catkin-tools \
-    python3-vcstool
+## catkin tools and other shit
+#sudo apt install \
+#    python-rosinstall \
+#    python-rosinstall-generator \
+#    python-wstool \
+#    build-essential \
+#    python-catkin-tools \
+#    python3-vcstool
+
+echo 'export ROS_PYTHON_VERSION=2' >> ${HOME}/.bashrc
