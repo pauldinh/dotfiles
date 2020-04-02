b94a967c17a8219abd76a381c3c864965281f4bc
Paul Dinh <paul.dinh@gmail.com>
2020-04-02 13:04:40 -0500

    Update sdcv aliases (add thes for thesaurus)

--- bash/bashrc.d/200.sdcv.sh
+++ bash/bashrc.d/200.sdcv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if which sdcv >/dev/null 2>&1; then
-    alias define='sdcv -c'
+    alias define='sdcv -c -u "Jargon File" -u "WordNet" -u "Free On-Line Dictionary of Computing" -u "dictd_www.dict.org_gcide" -u "Webster'\''s Revised Unabridged Dictionary (1913)"'
+    #alias define='sdcv -c -u "WordNet"'
+    alias thes='sdcv -c -u "Moby Thesaurus II"'
     alias jefine='sdcv -c -u "Jargon File"'
 fi
