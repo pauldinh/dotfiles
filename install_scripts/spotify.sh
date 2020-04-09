f1ab56de4514711aa6958c57b9a2901685f05617
Paul Dinh <paul.dinh@gmail.com>
2020-04-08 19:11:18 -0500

    Add spicetify themes

--- install_scripts/spotify.sh
+++ install_scripts/spotify.sh
@@ -5,9 +5,14 @@ cwd=$(pwd)
 sudo apt install flatpak
 flatpak install flathub com.spotify.Client
 
+# spice up spotify
 cd /tmp
 wget https://github.com/khanhas/spicetify-cli/releases/download/v0.9.7/spicetify-0.9.7-linux-amd64.tar.gz
 tar zxvf spicetify-0.9.7-linux-amd64.tar.gz -C ~/.spicetify
 ln -s ~/.spicetify/spicetify $HOME/.local/bin
+
+# themes
+git clone https://github.com/morpheusthewhite/spicetify-themes.git ~/.spicetify-themes --depth 1
+cp -r ~/.spicetify-themes/* ~/.config/spicetify/Themes
 
 cd ${cwd}
