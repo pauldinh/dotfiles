d801044b38813e0c6907964d4bf9b7d585942b6a
Paul Dinh <paul.dinh@gmail.com>
2020-04-19 07:15:37 -0500

    Add gifcast

---
+++ gifcast/00.install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://github.com/lolilolicon/FFcast
+
+# git clone --recursive https://github.com/lolilolicon/FFcast.git
+
+#./bootstrap  # generates ./configure
+#./configure --prefix /usr --libexecdir /usr/lib --sysconfdir /etc
+#make
+#make DESTDIR="$dir" install  # $dir must be an absolute path
+
+# if DESTDIR is /tmp/ffcast, output is /tmp/ffcast/usr/*
+# manually copying /tmp/ffcast/usr/bin to $HOME/.local/bin  etc
+# works but prob a better way
