bde550c553c9e916a87c607ab5a8788fb303a3b8
Paul Dinh <paul.dinh@gmail.com>
2020-04-19 12:15:23 -0500

    Update bash_history to also log commands to persistent log directory

--- bash/bashrc.d/010.bash_history.sh
+++ bash/bashrc.d/010.bash_history.sh
@@ -16,12 +16,19 @@ shopt -s checkwinsize
 shopt -s cmdhist
 
 # Record each line as it gets issued
-PROMPT_COMMAND='history -a'
+#PROMPT_COMMAND='history -a'
 
 # Don't record some commands
-export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
 
 # Use standard ISO 8601 timestamp
 # %F equivalent to %Y-%m-%d
 # %T equivalent to %H:%M:%S (24-hours format)
 HISTTIMEFORMAT='%F %T '
+
+# log *every* command to ~/.history in a timestamped log
+
+# PROMPT_COMMAND:
+# 1)    history -a: log each command when issued
+# 2) if <stuff> fi: log *every* command to timestamped log in ~/.bash_logs
+PROMPT_COMMAND='history -a;if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.bash_logs/bash-history-$(date "+%Y-%m-%d").log; fi'
