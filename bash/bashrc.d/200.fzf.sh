28e5445bffe1d5e3ae41699e3a01f52bbbdb1707
Paul Dinh <paul.dinh@gmail.com>
2020-04-19 20:51:06 -0500

    Update ros/fzf key bindings

--- bash/bashrc.d/200.fzf.sh
+++ bash/bashrc.d/200.fzf.sh
@@ -35,18 +35,37 @@ if [ -f ~/.fzf.bash ]; then
     # git fzf integration
     # all of these are bound to Ctrl Foo + Ctrl Bar, where Foo and Bar are keystrokes
     # all of these are bound to keystrokes (ignore first g) e.g. ggf is Ctrl G + Ctrl F
-
-    # Ctrl F + Ctrl T - *T*opics
     gft() {
-      rostopic list |
-      #fzf-down -m --ansi --nth 2..,.. --preview '(rostopic info {})'
-      fzf-down --no-height --info hidden --preview '(rostopic echo {} -n1)'
+        rostopic list |
+            fzf \
+                --prompt 'rostopic> ' \
+                --header '[enter] to paste | ctrl + [e]cho or [p]ub' \
+                --bind 'ctrl-e:execute[echo rostopic echo {}]+abort' \
+                --bind 'ctrl-p:execute[echo rostopic pub {} ; MSGTYPE=$(rostopic type {}); echo $MSGTYPE; ]+abort' \
+                --preview '(rostopic info {})'
     }
 
     # Ctrl F + Ctrl V - Ser*v*ics
     gfv() {
-      rosservice list |
-      fzf-down -m --ansi --nth 2..,.. --preview '(rosservice info {})'
+        rosservice list |
+            fzf \
+                --prompt 'rosservice> ' \
+                --header '[enter] to paste | ctrl + [p] to call' \
+                --bind 'ctrl-p:execute[echo rosservice call {}]+abort' \
+                --preview '(rosservice info {})'
+    }
+
+    # Ctrl F - Ctrl P - *Parameters
+    gfp() {
+      rosparam list |
+          fzf \
+              --prompt 'rosparam> ' \
+              --header '[enter] to paste | ctrl + [g]et or se[t] | [pgup/pgdn] to scroll preview'\
+              --bind 'ctrl-g:execute[echo rosparam get -p {}]+abort' \
+              --bind 'ctrl-t:execute[echo rosparam set {}]+abort' \
+              --bind 'pgup:preview-page-up' \
+              --bind 'pgdn:preview-page-down' \
+              --preview '(rosparam get -p {} | bat)'
     }
 
     # dirty files
@@ -127,6 +146,7 @@ if [ -f ~/.fzf.bash ]; then
 
       bind '"\C-f\C-t": "$(gft)\e\C-e\er"'
       bind '"\C-f\C-v": "$(gfv)\e\C-e\er"'
+      bind '"\C-f\C-p": "$(gfp)\e\C-e\er"'
     fi
 
 
