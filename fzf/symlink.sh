dea3c94e67df9d1dd6f7ad23d08109f70885df4b
Paul Dinh <paul.dinh@gmail.com>
2020-04-26 11:12:01 -0500

    Symlink notational notes and fzf snippets

--- fzf/symlink.sh
+++ fzf/symlink.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ln -sfv $HOME/dotfiles/fzf/fzfimg.sh $HOME/.local/bin/fzfimg  # TODO a921
+ln -sfv $HOME/Nextcloud/fzf/fzf-snippets $HOME/.fzf-snippets  # TODO a921
+ln -sfv $HOME/Nextcloud/notes $HOME/.notes                    # TODO a921
