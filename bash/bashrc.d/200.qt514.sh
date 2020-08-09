6749d346ab9d6adab9b0cacaaa930d477a7ff814
Paul Dinh <paul.dinh@gmail.com>
2020-08-08 20:30:02 -0500

    Add qt514 sourcing

--- bash/bashrc.d/200.qt514.sh
+++ bash/bashrc.d/200.qt514.sh
@@ -1,3 +1,5 @@
-#if [ -f /opt/qt514/bin/qt514-env.sh ] && ! $(echo ${PATH} | grep -q '/opt/qt514/bin'); then
-#    source /opt/qt514/bin/qt514-env.sh
-#fi
+if [ -f /opt/qt514/bin/qt514-env.sh ] && ! $(echo ${PATH} | grep -q '/opt/qt514/bin'); then
+    source /opt/qt514/bin/qt514-env.sh
+fi
+export QT_X11_NO_MITSHM=1
+export QT_GRAPHICSSYSTEM="native"
