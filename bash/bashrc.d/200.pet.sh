b509a069ff19c5a9f9d8f977f3bba4ffba809111
Paul Dinh <paul.dinh@gmail.com>
2021-03-31 09:07:08 -0500

    Add bashrc/pet

---
+++ bash/bashrc.d/200.pet.sh
@@ -0,0 +1,18 @@
+if which pet >/dev/null 2>&1; then
+
+    function pet-prev() {
+       PREV=$(echo `history | tail -n2 | head -n1` | sed 's/[0-9]* //')
+       sh -c "pet new `printf %q "$PREV"`"
+     }
+
+    function pet-select() {
+      BUFFER=$(pet search --query "$READLINE_LINE")
+      READLINE_LINE=$BUFFER
+      READLINE_POINT=${#BUFFER}
+    }
+
+    if [[ $- =~ i ]]; then
+        bind -x '"\C-x\C-r": "pet-select"'
+    fi
+
+fi
