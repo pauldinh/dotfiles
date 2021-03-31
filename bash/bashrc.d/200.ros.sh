c01f1436f931da4afe6d32e80ead983c83bc5fdc
Paul Dinh <paul.dinh@gmail.com>
2021-03-31 09:06:16 -0500

    Update bashrc/ros to export Python3 for Noetic

--- bash/bashrc.d/200.ros.sh
+++ bash/bashrc.d/200.ros.sh
@@ -1,20 +1,10 @@
-if [ -d /opt/ros/melodic ]; then
-    #export ROS_PYTHON_VERSION=3
+if [ -d /opt/ros/noetic ]; then
+    export ROS_PYTHON_VERSION=3
+    source /opt/ros/noetic/setup.bash
+elif [ -d /opt/ros/melodic ]; then
     source /opt/ros/melodic/setup.bash
 fi
 
 if [ -f /usr/share/vcstool-completion/vcs.bash ]; then
     source /usr/share/vcstool-completion/vcs.bash
-fi
-
-if [ -f ~/ros/ws/gizmo/install/setup.bash ]; then
-    source ~/ros/ws/gizmo/install/setup.bash
-fi
-
-if [ -f ~/ros/ws/overlay/devel/setup.bash ]; then
-    source ~/ros/ws/overlay/devel/setup.bash
-fi
-
-if [ -f ~/ros/ws/overlay/install/setup.bash ]; then
-    source ~/ros/ws/overlay/install/setup.bash
 fi
