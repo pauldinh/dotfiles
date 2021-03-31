c2ec5b19cca256047682496bc554071a9f4a4823
Paul Dinh <paul.dinh@gmail.com>
2021-03-31 01:34:20 -0500

    Update Nextcloud install script to use PPA instead of manually installing a deb

--- install_scripts/nextcloud.sh
+++ install_scripts/nextcloud.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cwd=$(pwd)
-
-cd /tmp
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
-sudo apt install ./ripgrep_12.1.1_amd64.deb
-
-cd ${cwd}
+sudo add-apt-repository ppa:nextcloud-devs/client
+sudo apt-get update
+sudo apt install nautilus-nextcloud
