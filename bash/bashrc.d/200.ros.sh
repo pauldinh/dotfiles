3aea581748dc62f70b976051e91c5b71d499c84e
Paul Dinh <paul.dinh@gmail.com>
2021-04-21 01:00:23 -0500

    200.ros.sh: source noetic/melodic iff ROS hasn't been sourced yet

--- bash/bashrc.d/200.ros.sh
+++ bash/bashrc.d/200.ros.sh
@@ -1,8 +1,10 @@
-if [ -d /opt/ros/noetic ]; then
-    export ROS_PYTHON_VERSION=3
-    source /opt/ros/noetic/setup.bash
-elif [ -d /opt/ros/melodic ]; then
-    source /opt/ros/melodic/setup.bash
+if [ -z "$ROS_PACKAGE_PATH" ]; then
+    if [ -d /opt/ros/noetic ]; then
+        export ROS_PYTHON_VERSION=3
+        source /opt/ros/noetic/setup.bash
+    elif [ -d /opt/ros/melodic ]; then
+        source /opt/ros/melodic/setup.bash
+    fi
 fi
 
 if [ -f /usr/share/vcstool-completion/vcs.bash ]; then
