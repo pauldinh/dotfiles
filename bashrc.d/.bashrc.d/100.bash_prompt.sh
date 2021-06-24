281912e024f3559de9445307da78bbea0f778d3a
Paul Dinh <paul.dinh@gmail.com>
2021-06-24 12:30:52 -0500

    Add red bash prompt color if root

--- bashrc.d/.bashrc.d/100.bash_prompt.sh
+++ bashrc.d/.bashrc.d/100.bash_prompt.sh
@@ -1,8 +1,20 @@
 if [ -f ~/.gitstatus/gitstatus.prompt.sh ]; then
     source ~/.gitstatus/gitstatus.prompt.sh
-    PS1='\[\033[01;32m\]\u@\h\[\033[00m\] '           # green user@host
-    PS1+='\[\033[01;34m\]\w\[\033[00m\]'              # blue current working directory
-    PS1+='${GITSTATUS_PROMPT:+ $GITSTATUS_PROMPT} '   # git status (requires promptvars option)
-    PS1+='\[\033[01;$((31+!$?))m\]\$\[\033[00m\] '    # green/red (success/error) $/# (normal/root)
-    PS1+='\[\e]0;\u@\h: \w\a\]'                       # terminal title: user@host: dir
+
+    _isroot=false
+    [[ "${UID}" -eq 0 ]] && _isroot=true
+
+    if ${_isroot}; then
+        _prompt_color_userhost="01;31"   # bold/light; red
+        _prompt_color_cwd="01;31"        # bold/light; red
+    else
+        _prompt_color_userhost="01;32"   # bold/light; green
+        _prompt_color_cwd="01;34"        # bold/light; blue
+    fi
+
+    PS1='\[\033[${_prompt_color_userhost}m\]\u@\h\[\033[00m\] ' # green user@host
+    PS1+='\[\033[${_prompt_color_cwd}m\]\w\[\033[00m\]'         # blue current working directory
+    PS1+='${GITSTATUS_PROMPT:+ $GITSTATUS_PROMPT} '             # git status (requires promptvars option)
+    PS1+='\[\033[01;$((31+!$?))m\]\$\[\033[00m\] '              # green/red (success/error) $/# (normal/root)
+    PS1+='\[\e]0;\u@\h: \w\a\]'                                 # terminal title: user@host: dir
 fi
