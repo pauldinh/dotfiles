99bbca3b599eab228395500ea2fe6acbacb5f993
Paul Dinh <paul.dinh@gmail.com>
2021-06-27 21:44:23 -0500

    Add 200.exa.sh

---
+++ bashrc.d/.bashrc.d/200.exa.sh
@@ -0,0 +1,12 @@
+if which exa >/dev/null 2>&1; then
+    alias ls='exa --group-directories-first'
+    alias ll='ls -l --git --all'
+
+    function lst() {
+        local depth=2
+        if [ ! -z "$1" ]; then
+            depth=$1
+        fi
+        exa --tree --level=$depth --git --long --all --group-directories-first
+    }
+fi
