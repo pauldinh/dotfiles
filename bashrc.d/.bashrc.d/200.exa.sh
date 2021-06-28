a50343c1c68edeb1e807029f3df7c07863228d20
Paul Dinh <paul.dinh@gmail.com>
2021-06-27 23:06:55 -0500

    Update 200.exa.sh with lsr alias

--- bashrc.d/.bashrc.d/200.exa.sh
+++ bashrc.d/.bashrc.d/200.exa.sh
@@ -1,6 +1,7 @@
 if which exa >/dev/null 2>&1; then
     alias ls='exa --group-directories-first'
     alias ll='ls -l --git --all'
+    alias lsr='ls -l --git --all --sort newest'
 
     function lst() {
         local depth=2
